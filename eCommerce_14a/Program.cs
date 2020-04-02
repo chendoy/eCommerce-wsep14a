104f49fd3abf2801b980e34fc4ca82d925dfa08f
chend <chendoy@post.bgu.ac.il>
2020-04-02 17:44:01 +0300

    Add project.

---
+++ eCommerce_14a/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace eCommerce_14a
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Store!");
+        }
+    }
+}
