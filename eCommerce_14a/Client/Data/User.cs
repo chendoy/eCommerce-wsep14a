b80edd4be83ae3008e565334af6669aa0bf35eb1
chendoy <chendoy@post.bgu.ac.il>
2020-04-25 21:59:10 +0300

    (broken) login screen added

---
+++ eCommerce_14a/Client/Data/User.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Client.Data
+{
+    public class User
+    {
+        [Required(ErrorMessage = "Username is required")]
+        public string Username { get; set; }
+        [Required(ErrorMessage = "Password is required")]
+        public string Password { get; set; }
+
+        public User()
+        {
+        }
+
+        public User(string Username, string Password)
+        {
+            this.Username = Username;
+            this.Password = Password;
+        }
+
+        public void clear()
+        {
+            this.Username = "";
+            this.Password = "";
+        }
+    }
+
+}
