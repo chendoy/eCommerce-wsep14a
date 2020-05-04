a4391a80d5f5cfc832dc7f7e3ddf650b65211cb5
chendoy <chendoy@post.bgu.ac.il>
2020-05-04 10:55:24 +0300

    added Register and Login response/request

---
+++ eCommerce_14a/Server/Communication/DataObject/RegisterResponse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.Communication.DataObject
+{
+    class RegisterResponse : Message
+    {
+        bool Success { get; set; }
+
+        public RegisterResponse(bool success) : base(Opcode.RESPONSE)
+        {
+            this.Success = Success;
+        }
+    }
+}
