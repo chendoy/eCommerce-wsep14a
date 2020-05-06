a85c6cd279fee0a5ced28f179b42ae2967f6a44b
Naor Kolet <naorko@post.bgu.ac.il>
2020-05-06 11:12:49 +0300

    Change websocket

--- eCommerce_14a/Client/Service/Communication.cs
+++ eCommerce_14a/Client/Service/Communication.cs
@@ -2,26 +2,55 @@
 using Microsoft.AspNetCore.Authentication;
 using System;
 using System.Linq;
-using System.Net.WebSockets;
 using System.Text.Json;
 using System.Threading;
 using System.Threading.Tasks;
+using WebSocketSharp;
+using System.Collections.Concurrent;
+using Server.Communication.DataObject;
+using System.Collections.Generic;
 
 namespace Client.Service
 {
     public class Communication
     {
-        private ClientWebSocket client;
+        private WebSocket client;
         private NetworkSecurity security;
         private const string PORT = "443";
+        private const string URL = "wss://localhost:" + PORT;
+        private BlockingCollection<string> responses; // store as json
 
         public Communication()
         {
-            client = new ClientWebSocket();
-            client.Options.AddSubProtocol("Tls");
-            client.ConnectAsync(new Uri("wss://localhost:"+PORT), new CancellationToken());
+            //client = new ClientWebSocket();
+            //client.Options.AddSubProtocol("Tls");
+            //client.ConnectAsync(new Uri("wss://localhost:"+PORT), new CancellationToken());
             
             security = new NetworkSecurity();
+
+            client = new WebSocket(URL, "Tls");
+            client.Connect();
+            client.OnMessage += Client_OnMessage;
+        }
+
+        private void Client_OnMessage(object sender, MessageEventArgs e)
+        {
+            byte[] byteMsg = e.RawData;
+            //byte[] ans = byteArr.TakeWhile((v, index) => byteArr.Skip(index).Any(w => w != 0x00)).ToArray();
+            string json = security.Decrypt(byteMsg);
+            Dictionary<string, object> resDict = JsonSerializer.Deserialize<Dictionary<string, object>>(json);
+            if (resDict.TryGetValue("_Opcode", out object opcodeObj))
+            {
+                int opcode = Convert.ToInt32(opcodeObj);
+                if (opcode == (int)Opcode.NOTIFICATION)
+                {
+
+                }
+                else
+                {
+                    responses.Add(json);
+                }
+            }
         }
 
         public void SendRequest(Object obj)
@@ -29,17 +58,14 @@ namespace Client.Service
             string json = JsonSerializer.Serialize(obj); // seralize this object into json string
             Console.WriteLine("sent: " + json);
             byte[] arr = security.Encrypt(json); // encrypt the string using aes algorithm and convert it to byte array
-            ArraySegment<byte> msg = new ArraySegment<byte>(arr); // init client msg
-            client.SendAsync(msg, WebSocketMessageType.Binary, true, new CancellationToken()); // send async the msg above to the server
+            //ArraySegment<byte> msg = new ArraySegment<byte>(arr); // init client msg
+            client.Send(arr);
+            //client.SendAsync(msg, WebSocketMessageType.Binary, true, new CancellationToken()); // send async the msg above to the server
         }
 
         public async Task<T> Get<T>()
         {
-            byte[] byteArr = new byte[1024];
-            ArraySegment<byte> buffer = new ArraySegment<byte>(byteArr); // init client msg
-            await client.ReceiveAsync(buffer, new CancellationToken());
-            byte[] ans = byteArr.TakeWhile((v, index) => byteArr.Skip(index).Any(w => w != 0x00)).ToArray();
-            string json = security.Decrypt(ans);
+            string json = responses.Take();
             T response = JsonSerializer.Deserialize<T>(json);
             return response;
         }
