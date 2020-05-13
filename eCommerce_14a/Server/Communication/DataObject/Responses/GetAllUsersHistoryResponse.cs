a823184fd979a35d1fe21ecd5f3c7b6aa3a94941
Guy Har Zahav <guyharzahav43@gmail.com>
2020-05-13 12:56:44 +0300

    added Allstores\UsersHIstory + StoreHistory

---
+++ eCommerce_14a/Server/Communication/DataObject/Responses/GetAllUsersHistoryResponse.cs
@@ -0,0 +1,21 @@
+﻿using Server.Communication.DataObject.ThinObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.Communication.DataObject.Responses
+{
+    class GetAllUsersHistoryResponse : Message
+    {
+        public Dictionary<string, List<PurchaseData>> Purchases { get; set; }
+        public string Error { get; set; }
+
+        public GetAllUsersHistoryResponse(Dictionary<string, List<PurchaseData>> purchases, string error) : base(Opcode.RESPONSE)
+        {
+            Purchases = purchases;
+            Error = error;
+        }
+    }
+}
