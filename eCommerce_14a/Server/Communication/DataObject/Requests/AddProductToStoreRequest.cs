7d582dbe7f32497ce1f4ba7432297cca6244edf6
chendoy <chendoy@post.bgu.ac.il>
2020-05-14 17:16:23 +0300

    add product working

--- eCommerce_14a/Server/Communication/DataObject/Requests/AddProductToStoreRequest.cs
+++ eCommerce_14a/Server/Communication/DataObject/Requests/AddProductToStoreRequest.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Server.Communication.DataObject.Requests
 {
-    class AddProductToStoreRequest : Message
+    public class AddProductToStoreRequest : Message
     {
         public int StoreId { get; set; }
         public string UserName { get; set; }
