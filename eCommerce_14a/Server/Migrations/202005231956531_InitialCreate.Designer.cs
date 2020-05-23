9e08208ed6725ac280cc6c3839dbbfdcbb2bbe7d
Liav <liavbapp@gmail.com>
2020-05-23 22:58:34 +0300

    Seeded purchase data and fixed cacde delete cycles to automatically handle it!

--- eCommerce_14a/Server/Migrations/202005231956531_InitialCreate.Designer.cs
+++ eCommerce_14a/Server/Migrations/202005231956531_InitialCreate.Designer.cs
@@ -13,7 +13,7 @@ namespace Server.Migrations
         
         string IMigrationMetadata.Id
         {
-            get { return "202005231726408_InitialCreate"; }
+            get { return "202005231956531_InitialCreate"; }
         }
         
         string IMigrationMetadata.Source
